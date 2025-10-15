4728da5cc46b6bef397a4aa8790ff699e75dbd40
Jan Hohenheim <jan@hohenheim.ch>
2025-10-15 05:12:43 +0200

    Revert "Remove WIP example"
    This reverts commit 796382084c88803cf3073a2af7257c40c4b0c7b6.

---
+++ examples/examples/avian.rs
@@ -0,0 +1,49 @@
+use avian_steam_audio::prelude::*;
+use avian3d::prelude::*;
+use bevy::{color::palettes::tailwind, prelude::*};
+use bevy_seedling::prelude::*;
+use bevy_steam_audio::{prelude::*, scene::mesh_backend::SteamAudioMesh};
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            SeedlingPlugin::default(),
+            PhysicsPlugins::default(),
+            SteamAudioPlugin::default(),
+            AvianSteamAudioScenePlugin,
+        ))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((Camera3d::default(), SteamAudioListener));
+
+    commands.spawn((
+        SamplePlayer::new(assets.load("selfless_courage.ogg")).looping(),
+        SteamAudioPool,
+        Transform::from_xyz(-1.5, 0.0, -3.0),
+        Mesh3d(meshes.add(Sphere::new(0.2))),
+        MeshMaterial3d(materials.add(Color::from(tailwind::GREEN_400))),
+    ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(0.1, 1.0, 3.0))),
+        MeshMaterial3d(materials.add(Color::from(tailwind::GRAY_600))),
+        Transform::from_xyz(1.0, 0.0, 0.0),
+        SteamAudioMesh::default(),
+        RigidBody::Static,
+        ColliderConstructor::TrimeshFromMesh,
+    ));
+
+    commands.spawn((
+        DirectionalLight::default(),
+        Transform::default().looking_to(Vec3::new(0.5, -1.0, -0.3), Vec3::Y),
+    ));
+}
