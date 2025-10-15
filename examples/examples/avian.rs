40a60c8152b02e4c2b37a1faa65916b399324e6a
Jan Hohenheim <jan@hohenheim.ch>
2025-10-15 05:12:23 +0200

    Tweak deps

--- examples/examples/avian.rs
+++ examples/examples/avian.rs
@@ -1,4 +1,5 @@
 use avian_steam_audio::prelude::*;
+use avian3d::prelude::*;
 use bevy::{color::palettes::tailwind, prelude::*};
 use bevy_seedling::prelude::*;
 use bevy_steam_audio::{prelude::*, scene::mesh_backend::SteamAudioMesh};
@@ -8,11 +9,8 @@ fn main() {
         .add_plugins((
             DefaultPlugins,
             SeedlingPlugin::default(),
-            // Add the SteamAudioPlugin to the app to enable Steam Audio functionality
+            PhysicsPlugins::default(),
             SteamAudioPlugin::default(),
-            // Steam Audio still needs some scene backend to know how to build its 3D scene.
-            // Mesh3dSteamAudioScenePlugin does this by using all entities that hold both
-            // `Mesh3d` and `MeshMaterial3d`.
             AvianSteamAudioScenePlugin,
         ))
         .add_systems(Startup, setup)
@@ -25,11 +23,8 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // The camera is our listener using SteamAudioListener
     commands.spawn((Camera3d::default(), SteamAudioListener));
 
-    // The sample player uses Steam Audio through the SteamAudioPool
-    // Let's place it to the front left of the listener, making direct sound come from the left
     commands.spawn((
         SamplePlayer::new(assets.load("selfless_courage.ogg")).looping(),
         SteamAudioPool,
@@ -38,13 +33,13 @@ fn setup(
         MeshMaterial3d(materials.add(Color::from(tailwind::GREEN_400))),
     ));
 
-    // Some occluding geometry using MeshSteamAudioMaterial
-    // Let's place it to the right of the listener, making reflected sound come from the right
     commands.spawn((
         Mesh3d(meshes.add(Cuboid::new(0.1, 1.0, 3.0))),
         MeshMaterial3d(materials.add(Color::from(tailwind::GRAY_600))),
         Transform::from_xyz(1.0, 0.0, 0.0),
         SteamAudioMesh::default(),
+        RigidBody::Static,
+        ColliderConstructor::TrimeshFromMesh,
     ));
 
     commands.spawn((
