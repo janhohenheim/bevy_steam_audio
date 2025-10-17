3fe36b5b11079e7386facac8b49134b0e12dad4f
Jan Hohenheim <jan@hohenheim.ch>
2025-10-17 14:15:53 +0200

    Fix lints

--- examples/examples/many.rs
+++ examples/examples/many.rs
@@ -1,9 +1,6 @@
 use std::time::Duration;
 
-use bevy::{
-    color::palettes::tailwind, input::common_conditions::input_just_pressed, prelude::*,
-    time::common_conditions::on_timer,
-};
+use bevy::{prelude::*, time::common_conditions::on_timer};
 use bevy_seedling::prelude::*;
 use bevy_steam_audio::{prelude::*, scene::mesh_backend::Mesh3dSteamAudioScenePlugin};
 
@@ -12,11 +9,7 @@ fn main() {
         .add_plugins((
             DefaultPlugins,
             SeedlingPlugin::default(),
-            // Add the SteamAudioPlugin to the app to enable Steam Audio functionality
             SteamAudioPlugin::default(),
-            // Steam Audio still needs some scene backend to know how to build its 3D scene.
-            // Mesh3dSteamAudioScenePlugin does this by using all entities that hold both
-            // `Mesh3d` and `SteamAudioMaterial`.
             Mesh3dSteamAudioScenePlugin::default(),
         ))
         .add_systems(Startup, setup)
@@ -26,7 +19,6 @@ fn main() {
 }
 
 fn setup(mut commands: Commands) {
-    // The camera is our listener using SteamAudioListener
     commands.spawn((Camera3d::default(), SteamAudioListener));
 }
 
@@ -44,6 +36,6 @@ fn spawn(mut commands: Commands, step: Res<Step>) {
     commands.spawn((
         SamplePlayer::new(step.0.clone()),
         SteamAudioPool,
-        Transform::from_xyz(-1.5, 0.0, -3.0),
+        Transform::default(),
     ));
 }
