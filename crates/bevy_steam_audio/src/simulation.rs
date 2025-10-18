1823c5b006f6e9c43b6aabc4127b9d93eb4af18e
Jan Hohenheim <jan@hohenheim.ch>
2025-10-18 04:54:03 +0200

    Improve error handling

--- crates/bevy_steam_audio/src/simulation.rs
+++ crates/bevy_steam_audio/src/simulation.rs
@@ -223,16 +223,18 @@ fn update_simulation(
     synchro: ResMut<AsyncSimulationSynchronization>,
     mut root: ResMut<SteamAudioRootScene>,
     mut nodes: Query<(&mut AudionimbusSource, &GlobalTransform, &SampleEffects)>,
-    mut ambisonic_node: Query<&mut SteamAudioNode>,
+    mut steam_audio_nodes: Query<&mut SteamAudioNode>,
     mut reverb_node: Single<&mut SteamAudioReverbNode, Without<EffectOf>>,
 
     pathing_settings: Res<SteamAudioPathingSettings>,
     probes: Option<Res<SteamAudioProbeBatch>>,
     time: Res<Time>,
+    mut errors: Local<Vec<String>>,
 ) -> Result {
     if !enabled.enabled {
         return Ok(());
     }
+    errors.clear();
     let listener_transform = listener.compute_transform();
     let listener_orientation = AudionimbusCoordinateSystem::from_bevy_transform(listener_transform);
     let shared_inputs = quality.to_audionimbus_simulation_shared_inputs(listener_orientation);
@@ -306,7 +308,13 @@ fn update_simulation(
             source_inputs(orientation),
         );
 
-        let mut node = ambisonic_node.get_effect_mut(effects)?;
+        let mut node = match steam_audio_nodes.get_effect_mut(effects) {
+            Ok(node) => node,
+            Err(err) => {
+                errors.push(format!("Failed to get Steam Audio node from source: {err}"));
+                continue;
+            }
+        };
         node.source_position = orientation;
         node.listener_position = listener_orientation;
     }
@@ -324,16 +332,28 @@ fn update_simulation(
 
     let Some(timer) = enabled.reflection_and_pathing_simulation_timer.as_mut() else {
         // User doesn't want any reflection or pathing simulation
-        return Ok(());
+        if errors.is_empty() {
+            return Ok(());
+        } else {
+            return Err(errors.join("\n").into());
+        }
     };
     timer.tick(time.delta());
     if !timer.is_finished() {
         // Not yet time to kick off expensive simulation
-        return Ok(());
+        if errors.is_empty() {
+            return Ok(());
+        } else {
+            return Err(errors.join("\n").into());
+        }
     }
     if !synchro.complete.load(Ordering::SeqCst) {
         // It's time, but the previous simulation is still running!
-        return Ok(());
+        if errors.is_empty() {
+            return Ok(());
+        } else {
+            return Err(errors.join("\n").into());
+        }
     }
 
     // The previous simulation is complete, so we can start the next one
@@ -357,7 +377,13 @@ fn update_simulation(
             audionimbus::SimulationFlags::REFLECTIONS | audionimbus::SimulationFlags::PATHING,
             source_inputs(orientation),
         );
-        let mut node = ambisonic_node.get_effect_mut(effects)?;
+        let mut node = match steam_audio_nodes.get_effect_mut(effects) {
+            Ok(node) => node,
+            Err(err) => {
+                errors.push(format!("Failed to get Steam Audio node from source: {err}"));
+                continue;
+            }
+        };
         node.pathing_available = probes.is_some();
     }
 
@@ -365,5 +391,9 @@ fn update_simulation(
     timer.reset();
     synchro.sender.send(())?;
 
-    Ok(())
+    if errors.is_empty() {
+        return Ok(());
+    } else {
+        return Err(errors.join("\n").into());
+    }
 }
